6606b06bbc1ae1f6e3b000559e5a2408b5f235b6
Phennylalanine <petergregoryj@gmail.com>
2025-05-15 09:06:30 +0900

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,6 +1,6 @@
 const questions = [
   { jp: "私たちはサッカーがすきです　‐ We like soccer.", en: "We like soccer." },
   { jp: "私はフランスへ行きたい - I want to go to France.", en: "I want to go to France." },
-  { jp: "僕たちはハンバーグを食べたい　－　We want to eat hamburger.", en: "We want to eat hamburger." },
+  { jp: "僕たちはハンバーグを食べたい　－　We want to eat hamburgers.", en: "We want to eat hamburgers." },
   // Add more here...
 ];
