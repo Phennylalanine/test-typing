47950d5ebfa25d821f590091321705e6bac065e0
Phennylalanine <petergregoryj@gmail.com>
2025-05-29 08:50:39 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 600;
+canvas.width = 550;
+canvas.height = 400;
 
 let player = {
   x: canvas.width / 2,
